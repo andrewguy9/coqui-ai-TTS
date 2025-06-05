da632fa4af296de73ffeb8def601978a9e78c2df
Andrew Thomson <athomsonguy@gmail.com>
2025-06-04 17:03:02 -0700

    add util files

---
+++ path_utils.py
@@ -0,0 +1,36 @@
+
+
+from pathlib import Path
+from typing import Generator
+
+
+def walk_paths(path_str: str) -> Generator[Path, None, None]:
+    path = Path(path_str)
+    for p in path.rglob('*'):
+        yield p
+
+def get_file_extension(path: Path):
+    return path.suffix[1:] if path.suffix else ''
+
+def is_audio(path_str):
+    ext = get_file_extension(path_str).lower()
+    return ext in ('wav', 'mp3')
+
+def is_normal_file(path: Path):
+    return path.is_file()
+
+def make_extension_replacer(new_ext: str):
+    if not new_ext.startswith('.'):
+        new_ext = '.' + new_ext
+    def replacer(path: Path) -> Path:
+        return path.with_suffix(new_ext)
+    return replacer
+
+def get_base_name(path: Path) -> str:
+    return path.stem
+
+def get_parent_path(path: Path) -> Path:
+    return path.parent
+
+def get_relative_path(context: Path, target: Path) -> Path:
+    return target.relative_to(context)
