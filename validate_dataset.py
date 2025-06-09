fca5bdb2591a8dd220587a4189163939c2a76ec3
Andrew Thomson <athomsonguy@gmail.com>
2025-06-09 16:19:57 -0700

    add dataset validation script.

---
+++ validate_dataset.py
@@ -0,0 +1,79 @@
+from typing import Iterable, Iterator, Tuple
+from docopt import docopt
+from functional_tools import identity, juxt, uniq
+from pathlib import Path
+
+import csv
+
+DatasetSample = Tuple[Path, str, str]  # (auto_file_stem, unnormalized text, normalized_text)
+def dataset_reader(dataset_path: Path) -> Iterator[DatasetSample]:
+    metadata_path = dataset_path / "metadata.csv"
+    wav_dir = dataset_path / "wavs"
+
+    if not metadata_path.exists() or not wav_dir.exists():
+        raise FileNotFoundError("Dataset metadata or wav directory does not exist.")
+
+    with metadata_path.open('r', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter="|")
+        for row in reader:
+            yield wav_dir / f"{row[0]}.wav", row[1], row[2]
+
+def validate_sample_text_length(r: DatasetSample) -> bool:
+    _, text, normalized = r
+    return len(text) < 250 and len(normalized) < 250
+
+from torchaudio import info
+
+def get_audio_length(r: DatasetSample) -> float:
+    audio_path, _, _ = r
+    metadata = info(audio_path)
+    return metadata.num_frames / metadata.sample_rate
+
+def validate_sample_audio_length(r: DatasetSample) -> bool:
+    max_audio_length = 11.6  # seconds
+    audio_path, _, _ = r
+    seconds = get_audio_length(r)
+    return seconds <= max_audio_length
+
+def validate_sample_conditioning_length(r: DatasetSample) -> bool:
+    min_conditioning_length = 3  # seconds
+    max_conditioning_length = 6  # seconds
+    audio_path, _, _ = r
+    seconds = get_audio_length(r)
+    return min_conditioning_length <= seconds <= max_conditioning_length
+
+def validate_all(r: DatasetSample) -> bool:
+    return (validate_sample_text_length(r) and
+            validate_sample_audio_length(r) and
+            validate_sample_conditioning_length(r))
+
+def validate_dataset_records(samples: Iterable[DatasetSample]) -> None:
+    samples = list(samples)
+    valid_text_lengt = list(map(validate_sample_text_length, samples))
+    valid_audio_length = list(map(validate_sample_audio_length, samples))
+    valid_conditioning_length = list(map(validate_sample_conditioning_length, samples))
+    all_valid = list(map(validate_all, samples))
+    pct_valid_text_length = sum(valid_text_lengt) / len(samples)
+    pct_valid_audio_length = sum(valid_audio_length) / len(samples)
+    pct_valid_conditioning_length = sum(valid_conditioning_length) / len(samples)
+    pct_all_valid = sum(all_valid) / len(samples)
+    print(f"Total samples: {len(samples)}")
+    print(f"Valid text length: {pct_valid_text_length:.2%}")
+    print(f"Valid audio length: {pct_valid_audio_length:.2%}")
+    print(f"Valid conditioning length: {pct_valid_conditioning_length:.2%}")
+    print(f"All valid: {pct_all_valid:.2%}")
+    
+def main(args):
+    output_path = Path(args['<source>'])
+
+    validate_dataset_records(dataset_reader(output_path))
+
+USAGE = """
+Combine directories to produce a dataset.
+
+Usage:
+  validate_dataset.py <source>
+"""
+if __name__ == "__main__":
+    args = docopt(USAGE)
+    main(args)
